abd2ab4fbfd799e5b2c820f5db2ce0bf65c5e6df
Walter Jessen <walter@walterjessen.com>
2023-11-20 14:03:12 -0500

    code updates

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from transformers import pipeline
 import base64
 from langchain.chains.summarize import load_summarize_chain
 from langchain.docstore.document import Document
-from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.document_loaders.pdf import PyMuPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from PyPDF2 import PdfReader
 import streamlit as st
@@ -16,7 +16,7 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, AutoModelForCausa
 
 # file loader and preprocessor
 def file_preprocessing(file, skipfirst, skiplast):
-    loader = PyPDFLoader(file)
+    loader = PyMuPDFLoader(file)
     pages = loader.load_and_split()
     print("")
     print("# pages[0] ##########")
@@ -40,26 +40,41 @@ def file_preprocessing(file, skipfirst, skiplast):
     print("# pages after loop ##########")
     print("")
     print(pages)
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=50)
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,  # number of characters
+        chunk_overlap=100,
+        length_function=len,
+        separators=["\n\n", "\n", " ", ""],  # default list
+    )
+    # https://dev.to/eteimz/understanding-langchains-recursivecharactertextsplitter-2846
     texts = text_splitter.split_documents(pages)
     final_texts = ""
     for text in texts:
-        #    print(text)
         final_texts = final_texts + text.page_content
     return final_texts
 
 
+def preproc_count(filepath, skipfirst, skiplast):
+    input_text = file_preprocessing(filepath, skipfirst, skiplast)
+    text_length = len(input_text)
+    return input_text, text_length
+
+
+def postproc_count(summary):
+    text_length = len(summary)
+    return text_length
+
+
 # llm pipeline
-def llm_pipeline(tokenizer, base_model, filepath, skipfirst, skiplast):
+def llm_pipeline(tokenizer, base_model, input_text):
     pipe_sum = pipeline(
         "summarization",
         model=base_model,
         tokenizer=tokenizer,
-        max_length=500,
+        max_length=600,
         min_length=300,
         truncation=True,
     )
-    input_text = file_preprocessing(filepath, skipfirst, skiplast)
     result = pipe_sum(input_text)
     result = result[0]["summary_text"]
     return result
@@ -85,35 +100,24 @@ def main():
     uploaded_file = st.file_uploader("Upload your PDF file", type=["pdf"])
     if uploaded_file is not None:
         st.subheader("Options")
-        col1, col2, col3, col4 = st.columns(4)
+        col1, col2, col3 = st.columns([1, 1, 2])
         with col1:
             model_names = [
-                "Flan T5 small",
-                "LaMini GPT-2 124M",
-                "Fine-Tuned T5 Small for Text Summarization",
+                "T5-Small",
+                "BART",
             ]
-            selected_model = st.radio("Select a model to use", model_names)
-            if selected_model == "LaMini GPT-2 124M":
-                checkpoint = "MBZUAI/LaMini-GPT-124M"
+            selected_model = st.radio("Select a model to use:", model_names)
+            if selected_model == "BART":
+                checkpoint = "ccdv/lsg-bart-base-16384-pubmed"
                 tokenizer = AutoTokenizer.from_pretrained(
                     checkpoint,
                     truncation=True,
                     legacy=False,
                     model_max_length=1000,
-                )
-                base_model = AutoModelForCausalLM.from_pretrained(
-                    checkpoint, torch_dtype=torch.float32
-                )
-            elif selected_model == "Fine-Tuned T5 Small for Text Summarization":
-                checkpoint = "Falconsai/text_summarization"
-                tokenizer = AutoTokenizer.from_pretrained(
-                    checkpoint,
-                    truncation=True,
-                    legacy=False,
-                    model_max_length=1000,
+                    trust_remote_code=True,
                 )
                 base_model = AutoModelForSeq2SeqLM.from_pretrained(
-                    checkpoint, torch_dtype=torch.float32
+                    checkpoint, torch_dtype=torch.float32, trust_remote_code=True
                 )
             else:  # default Flan T5 small
                 checkpoint = "MBZUAI/LaMini-Flan-T5-77M"
@@ -130,20 +134,36 @@ def main():
             st.write("Skip any pages?")
             skipfirst = st.checkbox("Skip first page")
             skiplast = st.checkbox("Skip last page")
+        with col3:
+            st.write("Background information (opens in a new window)")
+            st.write(
+                "Model class: [BART](https://huggingface.co/docs/transformers/main/en/model_doc/bart)"
+                "&nbsp;&nbsp;|&nbsp;&nbsp;Specific model: [MBZUAI/LaMini-Flan-T5-77M](https://huggingface.co/MBZUAI/LaMini-Flan-T5-77M)"
+            )
+            st.write(
+                "Model class: [T5-Small](https://huggingface.co/docs/transformers/main/en/model_doc/t5)"
+                "&nbsp;&nbsp;|&nbsp;&nbsp;Specific model: [ccdv/lsg-bart-base-16384-pubmed](https://huggingface.co/ccdv/lsg-bart-base-16384-pubmed)"
+            )
         if st.button("Summarize"):
             col1, col2 = st.columns(2)
             filepath = "data/" + uploaded_file.name
             with open(filepath, "wb") as temp_file:
                 temp_file.write(uploaded_file.read())
             with col1:
-                st.info("Uploaded PDF")
+                input_text, text_length = preproc_count(filepath, skipfirst, skiplast)
+                st.info(
+                    "Uploaded PDF&nbsp;&nbsp;|&nbsp;&nbsp;Number of words: "
+                    f"{text_length:,}"
+                )
                 pdf_viewer = displayPDF(filepath)
             with col2:
-                st.info("PDF Summary")
                 with st.spinner("Please wait..."):
-                    summary = llm_pipeline(
-                        tokenizer, base_model, filepath, skipfirst, skiplast
-                    )
+                    summary = llm_pipeline(tokenizer, base_model, input_text)
+                    text_length = postproc_count(summary)
+                st.info(
+                    "PDF Summary&nbsp;&nbsp;|&nbsp;&nbsp;Number of words: "
+                    f"{text_length:,}"
+                )
                 st.success(summary)
 
 
@@ -152,6 +172,15 @@ st.markdown(
 div[class*="stRadio"] > label > div[data-testid="stMarkdownContainer"] > p {
     font-size: 1rem;
     font-weight: 400;
+}
+div[class*="stMarkdown"] > div[data-testid="stMarkdownContainer"] > p {
+    margin-bottom: -15px;
+}
+div[class*="stCheckbox"] > label {
+    margin-bottom: -15px;
+}
+body > a {
+    text-decoration: underline;
 }
     </style>
     """,
